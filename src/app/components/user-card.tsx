2b45c5df51a36cfcbb9a69ceb38697aa069b7788
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-08-22 17:06:36 -0300

    feat: add animations

--- src/app/components/user-card.tsx
+++ src/app/components/user-card.tsx
@@ -1,22 +1,39 @@
 import { User } from '@/domain/entities/user'
 import { useUsers } from '../hooks/use-users'
+import { useTheme } from '../hooks/use-theme'
+import { useEffect, useState } from 'react'
 
 type UserCardProps = {
   user: User
+  delay: number
 }
 
-export function UserCard({ user }: UserCardProps) {
+export function UserCard({ user, delay }: UserCardProps) {
   const { deleteUser } = useUsers()
+  const { theme } = useTheme()
+  const [fade, setFade] = useState(false)
 
   const handleDelete = async () => {
     if (confirm('Are you sure you want to delete this user?'))
       await deleteUser(user.id)
   }
 
+  useEffect(() => {
+    setTimeout(() => {
+      setFade(true)
+    }, 100)
+  })
+
   return (
     <div
+      style={{
+        backgroundColor:
+          theme === 'light' ? 'rgba(255, 255, 255, 1)' : 'rgba(30, 30, 30, 1)',
+        color: theme === 'light' ? 'black' : 'white',
+        transitionDelay: `${delay * 100 + 200}ms`
+      }}
       onClick={handleDelete}
-      className="flex cursor-pointer flex-col items-center justify-center rounded-md bg-white p-4 drop-shadow-md transition-all duration-200 hover:scale-105 hover:bg-red-100 md:p-6"
+      className={`flex cursor-pointer flex-col items-center justify-center rounded-md p-4 drop-shadow-md transition-all duration-1000 md:p-6 ${fade ? 'opacity-100' : 'opacity-0'}`}
     >
       <h2 className="font-bold">{user.name}</h2>
       <p className="font-light">{user.email}</p>
